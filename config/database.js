5ea16e691ed0bd517d7a6726403b5ac11fb7b708
random developer <dublindeveloper@gmail.com>
2017-06-24 14:42:42 +0100

    updated app name, update to use remote mongodb database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/aggregator',
+    database: 'mongodb://admin:admin@ds137882.mlab.com:37882/aggregator_database',
     secret: 'yoursecret'
 };
+//local database- mongodb://localhost:27017/aggregator
